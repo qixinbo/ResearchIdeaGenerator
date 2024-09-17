8d3fd888f62db66703615de3af549ba5f0e004a0
qixinbo <qixinbo@gmail.com>
2024-09-17 13:12:42 +0800

    reorg

---
+++ InfoTracer/backend/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from fastapi import FastAPI
+from InfoTracer.frontend import ui
+
+app = FastAPI()
+
+ui.init(app)
+
+if __name__ == "__main__":
+    print(
+        'Please start the app with the "uvicorn" command as shown in the start.sh script'
+    )
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# use path of this example as working directory; enables starting this script from anywhere
+
+#current_dir=$(dirname "$0")
+current_dir=$(dirname "$(readlink -f "$0")")
+echo ${current_dir}
+export PYTHONPATH="${current_dir}":${PYTHONPATH}
+
+bash InfoTracer/backend/start_backend.sh "$1"
