7fd26267cd9578f9be6c45fcfce0e4ea60d0329d
qixinbo <qixinbo@gmail.com>
2024-10-07 12:13:14 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-cat IdeaGenerator/backend/requirements.txt IdeaGenerator/frontend/requirements.txt > requirements.txt
-pip install -r requirements.txt
+pip install -r IdeaGenerator/backend/requirements.txt
